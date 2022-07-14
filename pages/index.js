e7e5e6c05b4f46aaeb34e77a4d9eb1376011de64
bakemonostan <ehizomokhomion@gmail.com>
2022-07-14 02:06:08 -0700

    started herobanner component

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
+import { Product, FooterBanner, HeroBanner } from '../components';
+
 const Home = () => {
   return (
     <>
-      <div>HeroBanner</div>
+      <HeroBanner />
       <div>
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div>{['product 1', 'product 2'].map((product) => product)}</div>
-      Footer
+      <FooterBanner />
     </>
   );
 };
